ded81e6ee4f83f3fa7961ee76a6b62f4c79c6f37
thomasborde94 <thomas.borde94@hotmail.fr>
2024-01-17 10:50:11 +0100

    task 1, select plane, start game button, hide non-selected planes

--- unity-ar_slingshot_game/Assets/Scripts/GameManager.cs
+++ unity-ar_slingshot_game/Assets/Scripts/GameManager.cs
@@ -4,31 +4,27 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     #region Show in Inspector
     [SerializeField] private ARRaycastManager _raycastManager;
     [SerializeField] private ARPlaneManager _planeManager;
+    //[SerializeField] Material selectedMaterial;
 
-    [SerializeField] Material changedMaterial;
+    [SerializeField] GameObject _selectPlaneUI;
+    [SerializeField] GameObject _startGameUI;
     #endregion
+
     private void Awake()
     {
-        //session.Reset();
-    }
-
-    private void Start()
-    {
-        
+        _selectPlaneUI.SetActive(true);
+        _startGameUI.SetActive(false);
     }
 
     void Update()
     {
-        foreach (ARPlane plane in _planeManager.trackables)
-        {
-            plane.GetComponent<Renderer>().material = changedMaterial;
-        }
         if (Input.touchCount > 0 && selectedPlane == null && _planeManager.trackables.count > 0)
         {
             SelectPlane();
@@ -46,34 +42,23 @@ public class GameManager : MonoBehaviour
                 ARRaycastHit hit = hits[0];
                 selectedPlane = _planeManager.GetPlane(hit.trackableId);
 
-                //selectedPlane.GetComponent<Renderer>().material = PlaneOcclusionMaterial;
-                // SetMaterialTransparent(selectedPlane);
-
                 foreach (ARPlane plane in _planeManager.trackables)
                 {
                     if (plane != selectedPlane)
                     {
                         plane.gameObject.SetActive(false);
                     }
-                    else
-                        plane.GetComponent<Renderer>().material = changedMaterial;
+                    /*else
+                        plane.GetComponent<Renderer>().material = selectedMaterial;*/
                 }
-                _planeManager.enabled = false;
-                //selectPlaneCanvas.SetActive(false);
-                //OnPlaneSelected?.Invoke(selectedPlane);
+                _selectPlaneUI.SetActive(false);
+                _startGameUI.SetActive(true);
             }
         }
-
     }
 
-    #region Events
-    public delegate void PlaneSelectedEventHandler(ARPlane thePlane);
-    public event PlaneSelectedEventHandler OnPlaneSelected;
-    #endregion
-
     #region Private
-    private ARPlane selectedPlane = null;
-    private ARSession session;
+    private static ARPlane selectedPlane = null;
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
     #endregion
 }
