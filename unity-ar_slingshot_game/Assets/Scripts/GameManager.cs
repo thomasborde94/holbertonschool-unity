85b3b084922abe4033a68b9e8da9489e1d77c410
thomasborde94 <thomas.borde94@hotmail.fr>
2024-01-17 20:27:11 +0100

    spawn capsules, fix selected plane, capsules moving

--- unity-ar_slingshot_game/Assets/Scripts/GameManager.cs
+++ unity-ar_slingshot_game/Assets/Scripts/GameManager.cs
@@ -11,8 +11,12 @@ public class GameManager : MonoBehaviour
     #region Show in Inspector
     [SerializeField] private ARRaycastManager _raycastManager;
     [SerializeField] private ARPlaneManager _planeManager;
-    //[SerializeField] Material selectedMaterial;
+    [SerializeField] private Camera _camera;
 
+    [SerializeField] private GameObject _targetPrefab;
+    [SerializeField] private int _targetAmount;
+
+    [Header("UI")]
     [SerializeField] GameObject _selectPlaneUI;
     [SerializeField] GameObject _startGameUI;
     #endregion
@@ -21,18 +25,37 @@ public class GameManager : MonoBehaviour
     {
         _selectPlaneUI.SetActive(true);
         _startGameUI.SetActive(false);
+        _planeManager.enabled = true;
     }
 
     void Update()
     {
+        Ray rays = _camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(rays, out hit, Mathf.Infinity))
+        {
+            GameObject hitObject = hit.collider.gameObject;
+            Debug.Log("gameobject touché : " + hitObject.name);
+        }
+        Debug.DrawRay(rays.origin, rays.direction * 100f, Color.red );
+
+        /// -------- For Build -------- ///
         if (Input.touchCount > 0 && selectedPlane == null && _planeManager.trackables.count > 0)
         {
             SelectPlane();
         }
+        /// -------- For Editor ------- ///
+        /*
+        if (Input.GetMouseButtonDown(0) && selectedPlane == null && _planeManager.trackables.count > 0)
+        {
+            SelectPlane();
+        }
+        */
     }
 
     private void SelectPlane()
     {
+        /// ------- For Build ----- ///
         Touch touch = Input.GetTouch(0);
 
         if (touch.phase == TouchPhase.Began)
@@ -48,13 +71,48 @@ public class GameManager : MonoBehaviour
                     {
                         plane.gameObject.SetActive(false);
                     }
-                    /*else
-                        plane.GetComponent<Renderer>().material = selectedMaterial;*/
                 }
+                _planeManager.enabled = false;
                 _selectPlaneUI.SetActive(false);
                 _startGameUI.SetActive(true);
             }
         }
+        /// ---------- In Unity Editor ---------------- ///
+        /*Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            // Check if the hit object is an AR plane
+            ARPlane clickedPlane = hit.transform.GetComponent<ARPlane>();
+            selectedPlane = clickedPlane;
+            if (selectedPlane != null)
+            {
+                // Save the selected plane to the static variable in GameManager
+                
+                Debug.Log("selected plane is : " + selectedPlane.name);
+
+                foreach (ARPlane plane in _planeManager.trackables)
+                {
+                    if (plane != selectedPlane)
+                    {
+                        plane.gameObject.SetActive(false);
+                    }
+                }
+                _planeManager.enabled = false;
+                _selectPlaneUI.SetActive(false);
+                _startGameUI.SetActive(true);
+            }
+        }*/
+    }
+
+    public void StartGame()
+    {
+        for (int i = 1; i < _targetAmount; i++)
+        {
+            GameObject target = Instantiate(_targetPrefab, selectedPlane.center, selectedPlane.transform.rotation, selectedPlane.transform);
+            target.GetComponent<TargetMove>().StartMoving(selectedPlane);
+        }
+        _startGameUI.SetActive(false);
     }
 
     #region Private
---
+++ unity-ar_slingshot_game/Assets/Scripts/TargetMove.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class TargetMove : MonoBehaviour
+{
+    [SerializeField] private float speed = 0.01f;
+    
+
+    void Update()
+    {
+        if (!startMoving)
+            return;
+        if (hasDestination)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed);
+            if (Vector3.Distance(transform.position, destination) < 0.01f)
+            {
+                hasDestination = false;
+            }
+        }
+        else
+            hasDestination = RandomPoint(planeCenter, rayYoffset, range, out destination);
+    }
+    public void StartMoving(ARPlane plane)
+    {
+        movePlane = plane;
+        planeCenter = plane.center;
+        range = Mathf.Max(plane.size.x, plane.size.y);
+        rayYoffset = 0.5f;
+        colliderHeight = transform.localScale.y * GetComponent<CapsuleCollider>().height;
+        transform.position = planeCenter + Vector3.up * colliderHeight / 2;
+        hasDestination = RandomPoint(planeCenter, rayYoffset, range, out destination);
+        startMoving = true;
+    }
+
+    /*public void StopMoving()
+    {
+        startMoving = false;
+    }*/
+    public bool RandomPoint(Vector3 center, float rayYoffset, float range, out Vector3 result)
+    {
+        Vector3 next = center + Random.insideUnitSphere * range;
+        RaycastHit hit;
+        if (Physics.Raycast(next, Vector3.down, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.gameObject == movePlane.gameObject)
+            {
+                result = hit.point + Vector3.up * colliderHeight / 2;
+                return true;
+            }
+        }
+        result = Vector3.zero;
+        return false;
+    }
+
+    private float rayYoffset;
+    private Vector3 planeCenter;
+    private float range;
+    private bool hasDestination;
+    private Vector3 destination;
+    private bool startMoving = false;
+    private ARPlane movePlane;
+    private float colliderHeight;
+}
